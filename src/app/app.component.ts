638d33d704c67b455a20dbe410162a90cd8b1a04
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-19 19:14:35 +0300

    Добавил описание задачи

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatRadioModule } from "@angular/material/radio";
+import {MatExpansionModule} from "@angular/material/expansion";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatRadioModule],
+  imports: [RouterOutlet, MatRadioModule, MatExpansionModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
