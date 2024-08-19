8a0d4900d3f0b383011b1b209be8e25ee106f51d
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-20 02:23:55 +0300

    Поправил вывод табличных данных

--- src/app/country-list/country-list.component.ts
+++ src/app/country-list/country-list.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-country-list',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule],
   templateUrl: './country-list.component.html',
   styleUrl: './country-list.component.scss'
 })
 export class CountryListComponent {
+  displayedColumns = ['id', 'name', 'code', 'currencyCode'];
 
+  dataSource = [
+    { id : 'Q222', name : 'Afghanistan', code : 'AF', currencyCode : ['ALL, EUR']},
+    { id : 'Q222', name : 'Afghanistan', code : 'AF', currencyCode : ['ALL, EUR']},
+    { id : 'Q222', name : 'Afghanistan', code : 'AF', currencyCode : ['ALL, EUR']},
+    { id : 'Q222', name : 'Afghanistan', code : 'AF', currencyCode : ['ALL, EUR']},
+    { id : 'Q222', name : 'Afghanistan', code : 'AF', currencyCode : ['ALL, EUR']},
+    { id : 'Q222', name : 'Afghanistan', code : 'AF', currencyCode : ['ALL, EUR']},
+    { id : 'Q222', name : 'Afghanistan', code : 'AF', currencyCode : ['ALL, EUR']},
+    { id : 'Q222', name : 'Afghanistan', code : 'AF', currencyCode : ['ALL, EUR']},
+  ];
+
+  applyFilter(event : any)
+  {
+
+  }
 }
