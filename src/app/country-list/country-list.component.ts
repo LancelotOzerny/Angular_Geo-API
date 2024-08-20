ad9ca9ea599c54e563c31abcbbdc33cc4ba8489f
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-20 04:20:57 +0300

    Вывел страны в таблицу

--- src/app/country-list/country-list.component.ts
+++ src/app/country-list/country-list.component.ts
@@ -1,32 +1,48 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatSort, MatSortModule} from '@angular/material/sort';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {GeoService} from "../services/geo.service";
+import {HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-country-list',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule],
+  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule, HttpClientModule],
   templateUrl: './country-list.component.html',
-  styleUrl: './country-list.component.scss'
+  styleUrl: './country-list.component.scss',
+  providers: [GeoService],
 })
-export class CountryListComponent {
-  displayedColumns = ['id', 'name', 'code', 'currencyCode'];
+export class CountryListComponent implements OnInit
+{
+  displayedColumns = ['wikiDataId', 'name', 'code', 'currencyCodes'];
 
   dataSource = [
-    { id : 'Q222', name : 'Afghanistan', code : 'AF', currencyCode : ['ALL, EUR']},
-    { id : 'Q222', name : 'Afghanistan', code : 'AF', currencyCode : ['ALL, EUR']},
-    { id : 'Q222', name : 'Afghanistan', code : 'AF', currencyCode : ['ALL, EUR']},
-    { id : 'Q222', name : 'Afghanistan', code : 'AF', currencyCode : ['ALL, EUR']},
-    { id : 'Q222', name : 'Afghanistan', code : 'AF', currencyCode : ['ALL, EUR']},
-    { id : 'Q222', name : 'Afghanistan', code : 'AF', currencyCode : ['ALL, EUR']},
-    { id : 'Q222', name : 'Afghanistan', code : 'AF', currencyCode : ['ALL, EUR']},
-    { id : 'Q222', name : 'Afghanistan', code : 'AF', currencyCode : ['ALL, EUR']},
+    { wikiDataId : 'Q222', name : 'Afghanistan', code : 'AF', currencyCodes : ['ALL, EUR']},
+    { wikiDataId : 'Q222', name : 'Afghanistan', code : 'AF', currencyCodes : ['ALL, EUR']},
+    { wikiDataId : 'Q222', name : 'Afghanistan', code : 'AF', currencyCodes : ['ALL, EUR']},
+    { wikiDataId : 'Q222', name : 'Afghanistan', code : 'AF', currencyCodes : ['ALL, EUR']},
+    { wikiDataId : 'Q222', name : 'Afghanistan', code : 'AF', currencyCodes : ['ALL, EUR']},
+    { wikiDataId : 'Q222', name : 'Afghanistan', code : 'AF', currencyCodes : ['ALL, EUR']},
+    { wikiDataId : 'Q222', name : 'Afghanistan', code : 'AF', currencyCodes : ['ALL, EUR']},
+    { wikiDataId : 'Q222', name : 'Afghanistan', code : 'AF', currencyCodes : ['ALL, EUR']},
   ];
 
-  applyFilter(event : any)
+  constructor(private geoService : GeoService) {}
+
+  ngOnInit(): void
+  {
+    this.geoService.getCountries().subscribe({
+      next: (data : any) => {
+        console.log(data);
+        this.dataSource = data.data;
+      }
+    });
+  }
+
+  applyFilter(event : any) : void
   {
 
   }
