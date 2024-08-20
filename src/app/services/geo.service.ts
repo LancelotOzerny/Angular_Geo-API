34b153add20f1fac30d5bd57de1cf9b7d5c776d0
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-20 04:20:46 +0300

    Реализовал обращение к API по странам

---
+++ src/app/services/geo.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeoService {
+
+  constructor(private http: HttpClient) { }
+
+  private baseUrl : string = 'https://wft-geo-db.p.rapidapi.com/';
+
+  headersList : {} = {
+    'X-RapidAPI-Key': '4b1e9f6388msh81adb0a7d89cc10p1c108ejsnb1235ed7ff78',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  };
+
+  getCountries(limit : number = 5, offset : number = 0)
+  {
+    let paramsList = new HttpParams()
+      .set('limit', limit.toString())
+      .set('offset', offset.toString());
+
+    return this.http.get(this.baseUrl + 'v1/geo/countries', {
+      headers: this.headersList,
+      params: paramsList,
+    });
+  }
+}
