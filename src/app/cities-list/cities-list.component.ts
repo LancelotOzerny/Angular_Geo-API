8b7ff06c09c07885aba7f870e8ec4edc1062e98b
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-21 10:49:10 +0300

    Реализовал загрузку стран в dropdown

--- src/app/cities-list/cities-list.component.ts
+++ src/app/cities-list/cities-list.component.ts
@@ -1,29 +1,58 @@
-import { Component } from '@angular/core';
-
-import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {GeoService} from "../services/geo.service";
+import {HttpClientModule} from "@angular/common/http";
+import {LangComponent} from "../lang/lang.component";
+import {MatIconModule} from "@angular/material/icon";
+import {ListComponent} from "../list/list.component";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatSelectModule} from "@angular/material/select";
-import {MatInputModule} from "@angular/material/input";
-import {LangComponent} from "../lang/lang.component";
 
 @Component({
-  selector: 'app-cities-list',
+  selector: 'app-cites-list',
+  imports: [HttpClientModule, LangComponent, MatIconModule, MatFormFieldModule, MatSelectModule],
   standalone: true,
-  imports: [MatFormFieldModule, MatSelectModule, FormsModule, ReactiveFormsModule, MatInputModule, LangComponent],
-  templateUrl: './cities-list.component.html',
-  styleUrl: './cities-list.component.scss'
+  templateUrl: 'cities-list.component.html',
+  styleUrl: 'cities-list.component.scss',
+  providers: [ GeoService ]
 })
-export class CitiesListComponent
+export class CitiesListComponent extends ListComponent implements OnInit
 {
-  countriesSelectControl = new FormControl<{name : string, code : string} | null>(null, Validators.required);
-  selectFormControl = new FormControl('', Validators.required);
+  dataSource: {}[] = [];
+  displayedColumns: string[] = [];
+  selectCountriesList : { code : string, name : string }[] = [];
 
-  countriesData: {name : string, code : string}[] = [
-    {name: 'Afganistan', code: 'Q222'},
-    {name: 'Afganistan', code: 'Q222'},
-    {name: 'Afganistan', code: 'Q222'},
-    {name: 'Afganistan', code: 'Q222'},
-    {name: 'Afganistan', code: 'Q222'},
-    {name: 'Afganistan', code: 'Q222'},
-  ];
+  override ngOnInit()
+  {
+    super.ngOnInit();
+    this.fillCountriesList();
+  }
+
+  private fillCountriesList(offset = 0, limit = 5)
+  {
+    this.geoService.getCountries({ limit : limit, offset : offset }).subscribe({
+      next: (data : any) => {
+        for ( let element of data.data )
+        {
+          this.selectCountriesList.push({ code : element.wikiDataId, name : element.name });
+        }
+
+        if (data.metadata.totalCount > offset)
+        {
+          setTimeout(() => {
+            this.fillCountriesList(limit + offset);
+          }, 1000);
+        }
+      },
+      error: (data : any) => {
+        setTimeout(() => {
+          this.fillCountriesList(offset);
+        }, 1000);
+      }
+    });
+  }
+
+  protected override setListData() : void
+  {
+    this.selectCountriesList = [];
+  }
 }
