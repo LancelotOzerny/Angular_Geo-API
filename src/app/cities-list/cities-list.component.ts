be532c03db8353783c0bfcda6ecd71ee1566508d
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-21 20:20:03 +0300

    Реализовал выборку городов по коду страны, добавил none опцию.

--- src/app/cities-list/cities-list.component.ts
+++ src/app/cities-list/cities-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {GeoService} from "../services/geo.service";
 import {HttpClientModule} from "@angular/common/http";
 import {LangComponent} from "../lang/lang.component";
@@ -19,40 +19,62 @@ export class CitiesListComponent extends ListComponent implements OnInit
 {
   dataSource: {}[] = [];
   displayedColumns: string[] = [];
-  selectCountriesList : { code : string, name : string }[] = [];
+  selectCountriesList : { code : string, name : string }[] = [
+    { code: 'Q222', name : 'albania' },
+    { code: 'Q889', name : 'Afghanistan' },
+    { code: 'Q228', name : 'Andorra' },
+  ];
+
+  private countryCode = 'none';
 
   override ngOnInit()
   {
+    this.queryData = {
+      ...this.queryData,
+      countryCode: ''
+    };
     super.ngOnInit();
-    this.fillCountriesList();
+    // this.fillCountriesList();
+  }
+
+  selectChange(event : any)
+  {
+    this.queryData.countryCode = event.value;
+    this.setListData();
   }
 
   private fillCountriesList(offset = 0, limit = 5)
   {
-    this.geoService.getCountries({ limit : limit, offset : offset }).subscribe({
-      next: (data : any) => {
-        for ( let element of data.data )
+    this.geoService.getCountries({ limit: limit, offset: offset }).subscribe({
+      next: (data: any) => {
+        for (let element of data.data)
         {
-          this.selectCountriesList.push({ code : element.wikiDataId, name : element.name });
+          this.selectCountriesList.push({ code: element.wikiDataId, name: element.name });
         }
 
-        if (data.metadata.totalCount > offset)
+        const newOffset = offset + limit;
+        if (newOffset < data.metadata.totalCount)
         {
-          setTimeout(() => {
-            this.fillCountriesList(limit + offset);
-          }, 1000);
+          // Вызвать еще раз
         }
       },
-      error: (data : any) => {
-        setTimeout(() => {
-          this.fillCountriesList(offset);
-        }, 1000);
+      error: (error: any) => {
+        // Вызвать еще раз если ошибка 429
       }
     });
   }
 
   protected override setListData() : void
   {
-    this.selectCountriesList = [];
+    if (this.queryData.countryCode === 'none')
+    {
+      return;
+    }
+
+    this.geoService.getCities(this.queryData).subscribe({
+      next: (data : any) => {
+        console.log(data);
+      }
+    });
   }
 }
