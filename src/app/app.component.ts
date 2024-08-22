b4be3ba11f9a7807f0dee22ffe14b77de81eb388
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-08-22 03:47:43 +0300

    Добавил ГМ для перехода между сценами

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import { MatRadioModule } from "@angular/material/radio";
 import {MatExpansionModule} from "@angular/material/expansion";
 import {LangComponent} from "./lang/lang.component";
 import {LanguageService} from "./services/language.service";
 import {TaskComponent} from "./task/task.component";
+import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatRadioModule, MatExpansionModule, LangComponent, TaskComponent],
+  imports: [RouterOutlet, MatRadioModule, MatExpansionModule, LangComponent, TaskComponent, RouterLink, MatMenu, MatButtonModule, MatMenuItem, MatMenuTrigger],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [ LanguageService ],
